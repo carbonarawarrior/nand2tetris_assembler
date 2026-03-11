5aa110ed32332eb3a63c8ee562e7d24031fc5915
theodore padden <carbonarawarrior@proton.me>
2026-03-11 12:05:29 -0400

    I think this works as a file commit, IO cleanup and such.

--- src/com/Asm/Main.java
+++ src/com/Asm/Main.java
@@ -9,8 +9,46 @@ import java.io.FileWriter;
 public class Main {
 
     public static void main(String[] args) {
-	String asmFileName = args[0];
-	String hackFileName = asmFileName.replace(".asm", ".hack");
+	if (args.length < 1) {
+	    System.err.println("You must provide an assembly file!");
+	    System.exit(-1);
+	}
+
+	String asmFileName = "";
+	String hackFileName = "";
+	//i want to be able to add some options so we have to loop through our array to do so
+	for (int i = 0; i < args.length; i++) {
+	    switch (args[i]) {
+		case "-o":
+		    if (i + 1 >= args.length || args[i + 1].charAt(0) == '-') {
+			System.err.println("You must provide a name to write out to!");
+			System.exit(-1);
+		    }
+		    
+		    if (!hackFileName.isEmpty()) {
+			System.err.println("You have already specified a file to write out to!");
+			System.exit(-1);
+		    }
+		    
+		    hackFileName = args[i+1];
+		    i++; //skip the next file so its not treated as input
+		    break;
+		default:
+		    if (!asmFileName.isEmpty()) {
+			System.err.println("Too many input files!");
+			System.exit(-1);
+		    }
+		    //we assume that it is a .asm file
+		    asmFileName = args[i];
+		    break;
+	    }
+	}
+
+
+	if (hackFileName.isEmpty()) {
+	    hackFileName = asmFileName.replace(".asm", ".hack");
+	}
+
 	try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(hackFileName)))){
 	    Parser p = new Parser(new File(asmFileName));
 	    Code c = new Code();
